739884d9f06720ad53c3558abb1c188ecd0f7e7c
mpus <mati1900@gmail.com>
2024-04-23 20:19:53 +0200

    discrete approximation of US population done

--- lab5.py
+++ lab5.py
@@ -1,39 +1,58 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def approximation(years, vals, m):
-    n = len(years)
+YEARS = np.array([1900, 1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980],\
+                  dtype="float64")
+POPULATION = np.array([76212168, 92228496, 106021537, 123202624, 132164569,\
+                 151325798, 179323175, 203302031, 226542199], dtype="float64")
+REAL_90 = 248709873
+
+def approximation(m):
+    n = len(YEARS)
     A = np.zeros((n, (m + 1)), dtype="float64")
 
     for i in range(n):
         for j in range(m + 1):
-            A[i][j] = years[i] ** j
+            A[i][j] = YEARS[i] ** j
 
-    c = np.linalg.inv(A.T @ A) @ A.T @ vals
+    c = np.linalg.inv(A.T @ A) @ A.T @ POPULATION
     pol = np.polynomial.Polynomial(c)
 
     return pol
-    
 
-years = np.array([1900, 1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980],\
-                  dtype="float64")
-vals = np.array([76212168, 92228496, 106021537, 123202624, 132164569,\
-                 151325798, 179323175, 203302031, 226542199], dtype="float64")
-REAL90 = 248709873
+def AIC(m):
+    n = len(YEARS)
+    k = m + 1
+    pol = approximation(m)
 
-for i in range(7):
-    pol = approximation(years, vals, i)
-    estimated = pol(1990)
+    aic_val = 2 * k + n * np.log(np.sum((POPULATION - pol(YEARS)) ** 2))
+    aic_val += (2 * k * (k + 1)) / (n - k - 1)
+    return aic_val
 
-    s = f"m={i} 1990: {round(estimated)}"
-    s += f" err: {round(abs(REAL90 - estimated) / REAL90 * 100, 2)}%"
-    print(s)
+def zad1(plot = False):
+    for i in range(7):
+        pol = approximation(i)
+        estimated = pol(1990)
+
+        s = f"m={i} val: {round(estimated)}"
+        s += f" err: {round(abs(REAL_90 - estimated) / REAL_90 * 100, 2)}%"
+        print(s)
+
+    for i in range(7):
+        if i == 0:
+            print()
+        print(f"m={i} AIC = {round(AIC(i), 2)}")
+
+    if plot:
+        m = 2
+        pol = approximation(m)
+        plt.scatter(YEARS, POPULATION)
+        plt.scatter(1990, pol(1990))
+        x = np.linspace(1900, 1990, 1000)
+        y = pol(x)
+        
+        plt.plot(x, y)
+        plt.show()
 
 
-#plt.scatter(years, vals)
-#plt.scatter(1990, pol(1990))
-#x = np.linspace(1900, 1990, 1000)
-#y = pol(x)
-
-#plt.plot(x, y)
-#plt.show()
+zad1()
